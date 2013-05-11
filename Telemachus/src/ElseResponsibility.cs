987254b2221eb0f81e97127d1f72e2f7208e1fa2
Richard Bunt <bunt.rich@googlemail.com>
2013-05-11 20:03:24 +0100

    removed cyrpto reference;replaced redirect with helpful 404

--- Telemachus/src/ElseResponsibility.cs
+++ Telemachus/src/ElseResponsibility.cs
@@ -7,51 +7,27 @@ namespace Telemachus
 {
     class ElseResponsibility : IHTTPRequestResponsibility
     {
+        #region Constants
+
+        protected string INDEX_PAGE = "telemachus/information.html";
+
+        #endregion
+
         #region IHTTPRequestResponsibility
 
         public bool process(Servers.AsynchronousServer.ClientConnection cc, HTTPRequest request)
         {
-            ((Servers.MinimalHTTPServer.ClientConnection)cc).Send(new IOLessDataLinkRedirectToApplication());
-            return true;
-        }
-
-        #endregion
-    }
-
-    class IOLessDataLinkRedirectToApplication : HTTPResponse
-    {
-        #region Constants
-
-        const String HTML_FILE_SUFFIX = ".html";
-
-        #endregion
-
-        #region Constructors
-
-        public IOLessDataLinkRedirectToApplication()
-        {
-            protocol = "HTTP/1.0";
-            responseType = "Moved Permanently";
-            responseCode = "301";
-            attributes.Add("Location", "/telemachus/information.html");
-
-            if (!KSP.IO.FileInfo.CreateForType<TelemachusDataLink>("information.html").Exists)
+            if (!KSP.IO.FileInfo.CreateForType<TelemachusDataLink>(INDEX_PAGE).Exists)
             {
-                throw new ExceptionResponsePage("Unable to find the Telemachus index page. Is it installed in the PluginData folder?");
+                throw new PageNotFoundResponsePage("Unable to find the Telemachus index page. Is it installed in the PluginData folder?");
+            }
+            else
+            {
+                throw new PageNotFoundResponsePage(
+                    "Did you mean to visit the <a href=\"/" + INDEX_PAGE + "\">index page</a>?");
             }
         }
 
         #endregion
-
-        #region Page Generation
-
-        public override String ToString()
-        {
-            StringBuilder response = new StringBuilder(base.ToString());
-
-            return response.ToString();
-        }
-
-        #endregion
-    }  
+    }
 }
--- Telemachus/src/IOPageResponsibility.cs
+++ Telemachus/src/IOPageResponsibility.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Servers.MinimalHTTPServer;
-using System.Security.Cryptography;
 
 namespace Telemachus
 {
