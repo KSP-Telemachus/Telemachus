422b5c20742b09c500c77597cc7f7d3fdb3e965d
Richard Bunt <bunt.rich@googlemail.com>
2014-01-11 19:11:34 +0000

    updated version number; cleaned up TelemachusTest; added link to console.html on the landing page.

--- TelemachusTest/Program.cs
+++ TelemachusTest/Program.cs
@@ -13,7 +13,6 @@ namespace TelemachusTest
         static void Main(string[] args)
         {
             time(2000);
-            //time2(2000);
         }
 
         static private void time(int numberOfTests)
@@ -46,38 +45,6 @@ namespace TelemachusTest
             {
                 Console.ReadLine();
             }
-        }
-
-        static private void time2(int numberOfTests)
-        {
-            Console.WriteLine("Starting time test: " + numberOfTests);
-            Stopwatch timer = new Stopwatch();
-
-            string address = string.Format(
-            "http://127.0.0.1:8085/telemachus/datalink?alt={0}",
-            Uri.EscapeDataString("v.altitude"));
-            string text;
-            using (WebClient client = new WebClient())
-            {
-                timer.Start();
-                for (int i = 0; i < numberOfTests; i++)
-                {
-                    try{
-                        text = client.DownloadString(address);
-
-                    }
-                    catch (Exception d)
-                    {
-                        Console.WriteLine(d.Message + " " + i);
-                     
-                    }
-                }
-                timer.Stop();
-            }
-
-            Console.WriteLine("Average request time: " + timer.ElapsedMilliseconds / ((double)(numberOfTests)) + " milliseconds.");
-            Console.ReadLine();
-
         }
     }
 }
