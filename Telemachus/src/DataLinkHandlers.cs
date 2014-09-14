11f2130b57445b2e703bd611f7a9350712d13288
Richard Bunt <bunt.rich@googlemail.com>
2014-09-14 15:40:58 +0100

    Added API to pause and unpause game.

--- Telemachus/src/DataLinkHandlers.cs
+++ Telemachus/src/DataLinkHandlers.cs
@@ -656,9 +656,28 @@ namespace Telemachus
                         UnityEngine.SendMessageOptions.DontRequireReceiver); return false;
                 },
                 "t.timeWarp", "Time Warp [int rate]", formatters.Default));
+
             registerAPI(new PlotableAPIEntry(
                 dataSources => { return Planetarium.GetUniversalTime(); },
                 "t.universalTime", "Universal Time", formatters.Default, APIEntry.UnitType.DATE));
+
+            registerAPI(new ActionAPIEntry(
+               dataSources =>
+               {
+                   TelemachusBehaviour.instance.BroadcastMessage("queueDelayedAPI", new DelayedAPIEntry(dataSources.Clone(),
+                       (x) => { FlightDriver.SetPause(true); return 0d; }),
+                       UnityEngine.SendMessageOptions.DontRequireReceiver); return false;
+               },
+               "t.pause", "Pause game", formatters.Default));
+
+            registerAPI(new ActionAPIEntry(
+                dataSources =>
+                {
+                    TelemachusBehaviour.instance.BroadcastMessage("queueDelayedAPI", new DelayedAPIEntry(dataSources.Clone(),
+                        (x) => { FlightDriver.SetPause(false); return 0d; }),
+                        UnityEngine.SendMessageOptions.DontRequireReceiver); return false;
+                },
+                "t.unpause", "Unpause game", formatters.Default));
         }
 
         #endregion
