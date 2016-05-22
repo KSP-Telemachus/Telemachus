75e0ef174cb164b83fbcd435fa9a933064637169
Edward Cree <ec429@cantab.net>
2016-05-22 23:20:35 +0100

    Fix staging (KSP1.1 API change)

--- Telemachus/src/DataLinkHandlers.cs
+++ Telemachus/src/DataLinkHandlers.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Threading;
 using System.Collections;
 using UnityEngine;
+using KSP.UI.Screens;
 
 namespace Telemachus
 {
@@ -592,7 +593,7 @@ namespace Telemachus
                 dataSources =>
                 {
                     TelemachusBehaviour.instance.BroadcastMessage("queueDelayedAPI", new DelayedAPIEntry(dataSources.Clone(),
-                        (x) => { Staging.ActivateNextStage(); return 0d; }), UnityEngine.SendMessageOptions.DontRequireReceiver);
+						(x) => { StageManager.ActivateNextStage(); return 0d; }), UnityEngine.SendMessageOptions.DontRequireReceiver);
                     return predictFailure(dataSources.vessel);
                 }, "f.stage", "Stage", formatters.Default));
 
