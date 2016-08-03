989da10ebdf82677472cc8afd5e1ecf74fe78ec4
Thomas Cannon <hello@thomascannon.me>
2016-08-03 01:03:02 -0400

    WIP: Getting info from RasterPropMonitor External cameras
    This is a first stab at hooking up a custom module to
    JSIPrimitiveExternalCameras and grabbing info from the RasterPropMonitor
    module

---
+++ Telemachus/src/CameraSnapshots/RasterPropMonitorCamera.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telemachus.CameraSnapshots
+{
+    class RasterPropMonitorCamera : PartModule
+    {
+        
+
+        public string fieldValues()
+        {
+            string val = "";
+            foreach(BaseField field in Fields)
+            {
+                val += field.name + " : " + field.originalValue + " || ";
+            }
+
+            return val;
+        }
+
+        public string partModules()
+        {
+            string val = "";
+            foreach (PartModule module in part.Modules)
+            {
+                val += module.moduleName + " ||" ;
+            }
+
+            return val;
+        }
+
+        public string getRPMFields()
+        {
+            string val = "";
+            PartModule rpmModule = null;
+
+            foreach (PartModule module in part.Modules)
+            {
+                if(module.moduleName == "JSIExternalCameraSelector")
+                {
+                    rpmModule = module;
+                }
+            }
+
+            if(rpmModule == null)
+            {
+                return "NA";
+            }
+
+            foreach (BaseField field in rpmModule.Fields)
+            {
+                val += field.name + " : " + field.originalValue + " || ";
+            }
+
+            return val;
+        }
+
+        public void DebugInfo()
+        {
+            PluginLogger.debug("RPM CAMERA LOADED: " + part.name + " ;" + " ; POS: " + part.transform.position + " RPM FIELDS: " + getRPMFields() );
+        }
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            DebugInfo();
+        }
+
+        public void Update()
+        {
+            DebugInfo();
+        }
+    }
+}
