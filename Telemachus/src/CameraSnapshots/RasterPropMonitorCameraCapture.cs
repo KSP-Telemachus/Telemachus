ba941efb991bc7c13b4484be42558bac4ffb191e
Thomas Cannon <hello@thomascannon.me>
2016-08-05 22:12:47 -0400

    Fix rendering of RasterPropMonitor Cameras
    * Fix the update loop to skip transforming far-away cameras
    * Fix the update loop to translate camera positions

--- Telemachus/src/CameraSnapshots/RasterPropMonitorCameraCapture.cs
+++ Telemachus/src/CameraSnapshots/RasterPropMonitorCameraCapture.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace Telemachus.CameraSnapshots
@@ -10,6 +11,20 @@ namespace Telemachus.CameraSnapshots
     {
         public RasterPropMonitorCamera rpmCamera;
         protected static string cameraManagerNamePrefix = "RPMCamera:";
+        protected static readonly string[] camerasToSkipPositionTransform = { "GalaxyCamera", "Camera ScaledSpace", "Camera VE Underlay" };
+        protected Regex _cameraSkipRegex;
+        protected Regex cameraSkipRegex
+        {
+            get
+            {
+                if(_cameraSkipRegex == null)
+                {
+                    _cameraSkipRegex = new Regex("(" + String.Join("|", camerasToSkipPositionTransform) + ")$");
+                }
+
+                return _cameraSkipRegex;
+            }
+        }
 
         public override string cameraManagerName()
         {
@@ -37,13 +52,19 @@ namespace Telemachus.CameraSnapshots
 
         public override void additionalCameraUpdates(Camera cam)
         {
-            PluginLogger.debug("ROTATION:" + rpmCamera.rotateCamera);
-            cam.transform.position = rpmCamera.part.transform.position;
-            base.additionalCameraUpdates(cam);
-            PluginLogger.debug("PAST BASE");
+            if (!cameraSkipRegex.IsMatch(cam.name))
+            {
+                cam.transform.position = rpmCamera.part.transform.position;
+            }
+
+            // Just in case to support JSITransparentPod.
+            cam.cullingMask &= ~(1 << 16 | 1 << 20);
+
             cam.transform.rotation = rpmCamera.part.transform.rotation;
             cam.transform.Rotate(rpmCamera.rotateCamera);
-            //cam.transform.position += rpmCamera.translateCamera;
+            cam.transform.position += rpmCamera.translateCamera;
+            
+            base.additionalCameraUpdates(cam);
         }
     }
 }
